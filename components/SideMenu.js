0349e18168ffa3fe6b018b0c6c72945deba6d5a5
osol <sprivetom@inbox.ru>
2019-12-19 19:32:17 +0700

    styled Side menu component;

--- components/SideMenu.js
+++ components/SideMenu.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Drawer from '@material-ui/core/Drawer';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-import List from '@material-ui/core/List';
+import Drawer from "@material-ui/core/Drawer";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import List from "@material-ui/core/List";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import ChatIcon from "@material-ui/icons/Chat";
+import GroupIcon from "@material-ui/icons/Group";
+import ViewQuiltIcon from "@material-ui/icons/ViewQuilt";
+import AssignmentIcon from "@material-ui/icons/Assignment";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import SettingsIcon from "@material-ui/icons/Settings";
 
 const drawerWidth = 240;
 const useStyles = makeStyles(theme => ({
@@ -25,11 +31,20 @@ const useStyles = makeStyles(theme => ({
   drawerPaper: {
     width: drawerWidth
   },
-  toolbar: theme.mixins.toolbar,
   content: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.default,
     padding: theme.spacing(3)
+  },
+  avatarContainer: {
+    padding: "30px 0 30px 15px"
+  },
+  usernameContainer: {
+    padding: "0 0 20px 15px"
+  },
+  settingsSidemenuContainer: {
+    color: "grey",
+    padding: "10px 0 10px 15px"
   }
 }));
 
@@ -46,28 +61,56 @@ function SideMenu() {
         }}
         anchor="left"
       >
-        <div className={classes.toolbar} />
+        <div className={classes.avatarContainer}>
+          <Avatar />
+        </div>
+        <div className={classes.usernameContainer}>
+          <Typography variant="h6">Clava Coca</Typography>
+        </div>
         <Divider />
         <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
+          <ListItem button key={"Dashboard"}>
+            <ListItemIcon>
+              <ViewQuiltIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Dashboard"} />
+          </ListItem>
+          <ListItem button key={"Messages"}>
+            <ListItemIcon>
+              <ChatIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Messages"} />
+          </ListItem>
+          <ListItem button key={"My Tasks"}>
+            <ListItemIcon>
+              <AssignmentIcon />
+            </ListItemIcon>
+            <ListItemText primary={"My Tasks"} />
+          </ListItem>
+          <ListItem button key={"Clients"}>
+            <ListItemIcon>
+              <GroupIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Clients"} />
+          </ListItem>
         </List>
         <Divider />
+        <div className={classes.settingsSidemenuContainer}>
+          <Typography variant="subtitle1">Settings</Typography>
+        </div>
         <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
+          <ListItem button key={"Main Settings"}>
+            <ListItemIcon>
+              <SettingsIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Main Settings"} />
+          </ListItem>
+          <ListItem button key={"Notifications"}>
+            <ListItemIcon>
+              <NotificationsIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Notifications"} />
+          </ListItem>
         </List>
       </Drawer>
     </div>
