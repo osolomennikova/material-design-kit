3068161a0a30b9da0892f067ecf29dd2d528f6c7
osol <sprivetom@inbox.ru>
2019-12-19 18:22:49 +0700

    Add side bar;

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SideMenu from './components/SideMenu';
+
+function App() {
+    return (
+        <div>
+            <SideMenu />
+        </div>
+    )
+}
+
+export default App;
+
