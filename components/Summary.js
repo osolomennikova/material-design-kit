541633882babf37b6026cb3afdc1c47820117c34
osol <sprivetom@inbox.ru>
2019-12-20 13:14:05 +0700

    styled summary component;

--- components/Summary.js
+++ components/Summary.js
@@ -2,6 +2,11 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import GroupIcon from "@material-ui/icons/Group";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import AssignmentTurnedInIcon from '@material-ui/icons/AssignmentTurnedIn';
+import ReportIcon from '@material-ui/icons/Report';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -10,7 +15,12 @@ const useStyles = makeStyles(theme => ({
   paper: {
     padding: theme.spacing(2),
     textAlign: "center",
-    color: theme.palette.text.secondary
+    color: theme.palette.text.secondary,
+    '&:hover': {
+        background: "#2196f3",
+        cursor: 'pointer',
+        color: 'white'
+     },
   }
 }));
 
@@ -21,16 +31,32 @@ function Summary() {
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
+          <Paper className={classes.paper}>
+          <GroupIcon fontSize="large" />
+          <Typography variant="subtitle1">128</Typography>
+          <Typography variant="caption">Clients</Typography>
+          </Paper>
         </Grid>
         <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
+          <Paper className={classes.paper}>
+          <AssignmentTurnedInIcon fontSize="large" />
+          <Typography variant="subtitle1">32</Typography>
+          <Typography variant="caption">Completed tasks</Typography>
+          </Paper>
         </Grid>
         <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
+          <Paper className={classes.paper}>
+          <NotificationsIcon fontSize="large" />
+          <Typography variant="subtitle1">2</Typography>
+          <Typography variant="caption">Notifications</Typography>
+          </Paper>
         </Grid>
         <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
+          <Paper className={classes.paper}>
+          <ReportIcon fontSize="large" />
+          <Typography variant="subtitle1">4</Typography>
+          <Typography variant="caption">Reports</Typography>
+          </Paper>
         </Grid>
       </Grid>
     </div>
