2df8734af7a26b11391ddaac357a5d26a019f516
osol <sprivetom@inbox.ru>
2019-12-20 21:27:58 +0700

    add date picker component;

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import SideMenu from "./components/SideMenu";
 import Summary from "./components/Summary";
 import Grid from "@material-ui/core/Grid";
+import DatePicker from './components/DatePicker';
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
         <Grid container justify="space-between" spacing={3}>
           <Grid item xs="8">
             <Summary />
+          </Grid>
+          <Grid item xs="4">
+            <DatePicker />
           </Grid>
         </Grid>
       </main>
---
+++ components/DatePicker.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'react-daypicker/lib/DayPicker.css';
+import DayPicker from 'react-daypicker';
+
+
+function DatePicker() {
+    return (
+        <div>
+            <DayPicker />
+        </div>
+    )
+}
+
+export default DatePicker;
